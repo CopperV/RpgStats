b1d488c0ed3f7857ba0136b59eb4f983ff225eb8
CopperV <brosiak1999@gmail.com>
2026-07-04 09:37:32 +0200

    Added load stats events

---
+++ src/main/java/me/vark123/dsrpg/rpgStats/playerLogic/events/PlayerStatsLoadEvent.java
@@ -0,0 +1,18 @@
+package me.vark123.dsrpg.rpgStats.playerLogic.events;
+
+import fr.phoenixdevt.profiles.PlayerProfile;
+import lombok.Getter;
+import me.vark123.dsrpg.players.RpgPlayer;
+import me.vark123.dsrpg.players.events.APlayerActionEvent;
+import me.vark123.dsrpg.rpgStats.playerLogic.RpgPlayerStats;
+import org.bukkit.entity.Player;
+
+public class PlayerStatsLoadEvent extends APlayerActionEvent {
+    @Getter
+    private final RpgPlayerStats stats;
+
+    public PlayerStatsLoadEvent(Player player, PlayerProfile profile, RpgPlayer rpgPlayer, RpgPlayerStats stats) {
+        super(player, profile, rpgPlayer);
+        this.stats = stats;
+    }
+}
--- src/main/java/me/vark123/dsrpg/rpgStats/playerLogic/listeners/PlayerLoginStateListener.java
+++ src/main/java/me/vark123/dsrpg/rpgStats/playerLogic/listeners/PlayerLoginStateListener.java
@@ -3,6 +3,9 @@ package me.vark123.dsrpg.rpgStats.playerLogic.listeners;
 import me.vark123.dsrpg.players.events.RpgPlayerJoinEvent;
 import me.vark123.dsrpg.players.events.RpgPlayerLeaveEvent;
 import me.vark123.dsrpg.rpgStats.playerLogic.RpgPlayerStatsManager;
+import me.vark123.dsrpg.rpgStats.playerLogic.events.PlayerStatsLoadEvent;
+import me.vark123.dsrpg.rpgStats.playerLogic.events.PlayerStatsUnloadEvent;
+import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 
@@ -11,14 +14,30 @@ public class PlayerLoginStateListener implements Listener {
     @EventHandler
     private void onJoin(RpgPlayerJoinEvent e){
         var uuid = e.getRpgPlayer().getUuid();
-        RpgPlayerStatsManager.getInstance().loadStats(uuid);
+        var stats = RpgPlayerStatsManager.getInstance().loadStats(uuid);
+
+        if(stats != null){
+            var player = e.getPlayer();
+            var profile = e.getProfile();
+            var rpgPlayer = e.getRpgPlayer();
+            var event = new PlayerStatsLoadEvent(player, profile, rpgPlayer, stats);
+            Bukkit.getPluginManager().callEvent(event);
+        }
     }
 
     @EventHandler
     private void onLeave(RpgPlayerLeaveEvent e){
         var uuid = e.getRpgPlayer().getUuid();
         RpgPlayerStatsManager.getInstance().saveStats(uuid);
-        RpgPlayerStatsManager.getInstance().removeStats(uuid);
+        var stats = RpgPlayerStatsManager.getInstance().removeStats(uuid);
+
+        if(stats != null){
+            var player = e.getPlayer();
+            var profile = e.getProfile();
+            var rpgPlayer = e.getRpgPlayer();
+            var event = new PlayerStatsUnloadEvent(player, profile, rpgPlayer, stats);
+            Bukkit.getPluginManager().callEvent(event);
+        }
     }
 
 }
